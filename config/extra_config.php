f892e2497e38f0fc4dbe06f225c4bdc27503d0e9
vagrant <andrew@hurlinghamtravel.co.uk>
2019-07-31 16:03:21 +0000

    redirect after auth

--- config/extra_config.php
+++ config/extra_config.php
@@ -4,5 +4,5 @@ return [
 	'domain' => env('APP_DOMAIN', 'http://localhost.test'),
 	'client_secret' => env('CLIENT_SECRET'),
 	'client_id' => env('CLIENT_ID'),
-	'redirect_domain' => env('REDIRECT_URL'),
+	'redirect_domain' => env('APP_REDIRECT'),
 ];
